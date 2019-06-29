3bea013c3a871d94180c3a94429fa16600e7fbe9
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-06-29 16:48:35 +0300

    built basic editor and made link ffrom add button in calendar page to editor page

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,17 +1,20 @@
-import React,{Component, Fragment} from 'react';
-import {Link} from 'react-router-dom'
-import './Calendar.scss';
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import "./Calendar.scss";
 
 const Calendar = props => {
-return (
+  return (
     <Fragment>
-        <section className="add_container">
-            +
-        </section>
-        <section>
-            :)
-        </section>
+      <section className="add_container">
+        <Link to={`/editor`} className="add_link">
+          <p>+</p>
+        </Link>
+      </section>
+      <section>
+        <div className="face"> </div>
+      </section>
     </Fragment>
-)}
+  );
+};
 
 export default Calendar;
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,10 +2,45 @@ import React,{Component, Fragment} from 'react';
 import {Link} from 'react-router-dom'
 
 const Editor = props => {
+    const {date, onDate, mood, onMood} = props;
 return (
-    <div>
-      'Editor'
-    </div>
+    <form>
+        <fieldset>
+            <label htmlFor="date">Date</label>
+            <input 
+            type="date"
+            name="date"
+            id="date"
+            value={date}
+            onChange={onDate}
+            />
+        </fieldset>
+        <fieldset>
+            <label htmlFor="mood_happy">:)</label>
+            <input 
+            type="radio"
+            name="mood"
+            id="mood_happy"
+            value="happy"
+            onChange={onMood}
+            />
+            <label htmlFor="mood_sad">:(</label>
+            <input type="radio"
+            name="mood"
+            id="mood_sad"
+            value="sad"
+            onChange={onMood}/>
+        </fieldset>
+        <fieldset>
+            <label htmlFor="message">Message</label>
+            <input 
+            type="text"
+            name="message"
+            id="message"
+            value="¿Por qué es un buen día"
+            />
+        </fieldset>
+    </form>
 )}
 
 export default Editor;
